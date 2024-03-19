350e3fa333491e10484259fe351bd07807424cbf
yun <austru00@gmail.com>
2024-03-19 22:17:37 +0900

    weather.js codereview

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,9 @@ function onGeoOK(position) {
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
+    //JSON 데이터를 javascript 객체로 변환
     .then((response) => response.json())
+    //데이터 처리 부분
     .then((data) => {
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#weather span:last-child");
@@ -13,7 +15,9 @@ function onGeoOK(position) {
       weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
     });
 }
+
 function onGeoError() {
   alert("Can't find you. No weather for you.");
 }
+
 navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
