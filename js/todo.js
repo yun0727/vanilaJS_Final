8575d6b85ffff3b222e0a0f633c1c414e95eec56
yun <austru00@gmail.com>
2024-03-19 22:07:28 +0900

    todo.js code review

--- js/todo.js
+++ js/todo.js
@@ -6,13 +6,17 @@ const TODOS_KEY = "todos";
 
 let toDos = [];
 
+//할 일 목록을 로컬 스토리지에 저장하는 함수를 정의. 배열을 JSON 문자열로 변환하여 저장.
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
+//할 일을 삭제하는 함수를 정의. 클릭된 삭제 버튼이 속한 리스트 요소를 찾아서 삭제하고, 배열에서 해당 할 일을 제거한 후, 변경된 할 일 목록을 저장.
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  //선택된 할 일의 id와 일치하지 않는 모든 할 일이 새로운 배열로 만들어집니다. 
+  //parseInt() 함수를 사용하여 <li> 요소의 id를 정수로 변환하고, 이를 기반으로 필터링 작업을 수행
   toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
   saveToDos();
 }
